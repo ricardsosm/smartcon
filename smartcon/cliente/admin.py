d71641d351c4c891059650eca6ae8d2c38101222
Ricardo Martins <ricardo@servipec.com.br>
2019-01-14 12:02:03 -0200

    adicionando cliente no db

--- smartcon/cliente/admin.py
+++ smartcon/cliente/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from cliente.models import (Cliente)
 
-# Register your models here.
+class ClienteAdmin(admin.ModelAdmin):
+	
+	list_display = ['name','create_at','update_at']
+	search_field = ['name']
+
+admin.site.register(Cliente)
--- smartcon/usuario/admin.py
+++ smartcon/usuario/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-
-from .models import (Usuario)
+from cliente.models import (Usuario)
 
 class UsuarioAdmin(admin.ModelAdmin):
 	
